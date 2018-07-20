591275a49985d1beb898fbb92d2d3ac8111e21b3
Tomas Repel <trepel@redhat.com>
2018-07-20 14:23:57 +0200

    Fixing URL for app metrics integration test

--- core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
+++ core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
@@ -1,19 +1,28 @@
 package org.aerogear.mobile.core.integration;
 
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RuntimeEnvironment;
 
+import android.app.Application;
 import android.support.test.filters.SmallTest;
 
 import org.aerogear.mobile.core.AeroGearTestRunner;
 import org.aerogear.mobile.core.MobileCore;
+import org.aerogear.mobile.core.configuration.MobileCoreConfiguration;
+import org.aerogear.mobile.core.configuration.MobileCoreJsonParser;
+import org.aerogear.mobile.core.configuration.ServiceConfiguration;
 import org.aerogear.mobile.core.executor.AppExecutors;
 import org.aerogear.mobile.core.metrics.Metrics;
 import org.aerogear.mobile.core.metrics.MetricsService;
@@ -29,8 +38,22 @@ public class MetricsServiceIntegrationTest {
 
     @Before
     public void setUp() {
-        MobileCore.init(RuntimeEnvironment.application);
-        metricsService = new MetricsService("https://demo1623828.mockable.io/metrics");
+        Application context = RuntimeEnvironment.application;
+        MobileCore.init(context);
+
+        // read the app metrics endpoint from mobile-services.json
+        String metricsEndpoint = null;
+        try (InputStream configStream = context.getAssets().open("mobile-services.json")) {
+            MobileCoreConfiguration jsonConfig = new MobileCoreJsonParser(configStream).parse();
+            Map<String, ServiceConfiguration> configsPerId = jsonConfig.getServicesConfigPerId();
+            ServiceConfiguration metricsConfig = configsPerId.get("metrics-myapp-android");
+            metricsEndpoint = metricsConfig.getUrl();
+        } catch (JSONException | IOException exception) {
+            System.err.println(exception);
+            fail(exception.getMessage());
+        }
+
+        metricsService = new MetricsService(metricsEndpoint);
 
         error = null;
     }
